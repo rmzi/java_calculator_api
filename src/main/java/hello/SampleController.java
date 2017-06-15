8444f00e370d6e4c13b439ba1c963f5351f7664d
Ramzi Abdoch <ramzi_abdoch@mckinsey.com>
2017-06-15 10:19:36 -0700

    ResponseMapping not working, need fix

--- src/main/java/hello/SampleController.java
+++ src/main/java/hello/SampleController.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
+import org.json.*;
+
 @Controller
 @EnableAutoConfiguration
 public class SampleController {
@@ -13,6 +15,19 @@ public class SampleController {
   @ResponseBody
   String home() {
     return "Hello, world!";
+  }
+
+  @RequestMapping(value="/api/math/add", method = RequestMethod.POST)
+  public @ResponseBody String addition(@RequestBody String jsonString) {
+    JSONObject operands = new JSONObject(jsonString);
+    long num1 = Long.parseLong(operands.getString("num1"));
+    long num2 = Long.parseLong(operands.getString("num2"));
+    long result = num1 + num2;
+
+    // Sanity Check
+    System.out.println("num1: " + new Long(num1).toString() + "num2: " + new Long(num2).toString());
+
+    return "{'result':'" + result + "'}";
   }
 
   public static void main(String[] args) throws Exception {
